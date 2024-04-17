7f3252f0fea2540173959c50f59fd0ba0481da11
dsuarez <dsuarez@Lv15g2.unitech>
2024-04-16 23:28:09 -0300

    punto 11

---
+++ tp1_SuarezDario/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int aux=0;
+		int j,i=2;
+		for (j = 40; j > 0; j--) {
+			//System.out.println("j es: "+j);
+			System.out.println("total es: "+j*i);
+			i=i+1;
+			}
+		
+		
+	}
+
+}
